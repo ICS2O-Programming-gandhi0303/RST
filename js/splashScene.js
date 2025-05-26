082154da68594cb24a6e419d0eb85e4a51eddd36
Dushime Gandhi <dushime.gandhi@stu.ocsb.ca>
2025-05-26 14:17:15 +0000

    RST

--- js/splashScene.js
+++ js/splashScene.js
@@ -10,7 +10,7 @@ class SplashScene extends Phaser.Scene {
   
     preload() {
         console.log('Splash Scene')
-        this.load.image('splashSceneBackground', 'assets/splashSceneBackground.png');
+        this.load.image('splashSceneBackground', './assets/splashSceneBackground.png');
     }
   
     create(data) {
