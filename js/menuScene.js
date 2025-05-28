d2fa808f0e39c077a9342ccc6f9d42c173d1a7a4
Dushime Gandhi <dushime.gandhi@stu.ocsb.ca>
2025-05-28 15:13:35 +0000

    changed code

--- js/menuScene.js
+++ js/menuScene.js
@@ -1,4 +1,4 @@
-class MenuScene extends Phaser.Scene {
+class MenuScene extends Phaser.Scene { 
     constructor() {
       super({ key: "menuScene" })
       this.menuSceneBackgroundImage = null
@@ -10,8 +10,8 @@ class MenuScene extends Phaser.Scene {
     }
     preload() {
       console.log("Menu Scene")
-      this.load.image('menuSceneBackground', "./assets/aliens_screen_image2.png")
-      this.load.image('startButton', "./assets/start.png")
+      this.load.image("menuSceneBackground", "./assets/aliens_screen_image2.png")
+      this.load.image("startButton", "./assets/start.png")
     }
     create(data) {
       this.menuSceneBackgroundImage = this.add.sprite(0, 0, "menuSceneBackground")
@@ -24,8 +24,8 @@ class MenuScene extends Phaser.Scene {
     }
     update(time, delta) {
     }
-    clickButton() {
-      this.scene.start("gameScene")
+       clickButton() {
+       this.scene.start("gameScene")
     }
   }
   
