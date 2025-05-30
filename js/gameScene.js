f22bd782e3650392a27dffe535b26afc3fbfed5b
Dushime Gandhi <dushime.gandhi@stu.ocsb.ca>
2025-05-30 15:36:11 +0000

    Sentence about what you did

--- js/gameScene.js
+++ js/gameScene.js
@@ -39,6 +39,7 @@ class GameScene extends Phaser.Scene {
     this.background = this.add.image(0, 0, 'starBackground').setScale(2.0)
     this.background.setOrigin(0, 0)
     this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, 'ship')
+    this.ship.body.allowGravity = false
     this.missileGroup = this.physics.add.group()
     this.alienGroup = this.physics.add.group()
     // Ensure groups are initialized before creating aliens
