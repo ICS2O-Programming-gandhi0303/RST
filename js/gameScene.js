62e987073793972cd2463e2e43994bf577879a35
Dushime Gandhi <dushime.gandhi@stu.ocsb.ca>
2025-05-30 14:32:31 +0000

    Sentence about what you did

--- js/gameScene.js
+++ js/gameScene.js
@@ -1,4 +1,15 @@
 class GameScene extends Phaser.Scene {
+  createAlien() {
+    const alien = this.physics.add.sprite(1920 / 2, 100, 'alien')
+    alien.setVelocityY(50)
+    alien.setCollideWorldBounds(true)
+    alien.setBounce(1)
+    this.alienGroup.add(alien)
+    this.physics.add.collider(this.ship, this.alienGroup, function(ship, alien) {
+      console.log('Ship collided with alien!')
+      alien.destroy() 
+    })
+  }
   constructor() {
     super({ key: 'gameScene', physics: { default: 'arcade' } })
 
@@ -19,22 +30,26 @@ class GameScene extends Phaser.Scene {
     this.load.image("starBackground", "./assets/starBackground.png")
     this.load.image("ship", "./assets/spaceShip.png")
     this.load.image("missile", "./assets/missile.png")
+    this.load.image("alien", "./assets/alien.png")
+    // Load the sound effect for the laser
     this.load.audio("laser", "./assets/laser1.wav")
   }
 
-  create() {
+  create(data) {
     this.background = this.add.image(0, 0, 'starBackground').setScale(2.0)
     this.background.setOrigin(0, 0)
 
     this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, 'ship')
     this.missileGroup = this.physics.add.group()
+    this.alienGroup = this.add.group()
+    this.createAlien()
 
     // Set up keyboard cursors
     this.cursors = this.input.keyboard.createCursorKeys()
     this.keySpaceObj = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE)
   }
 
-  update() {
+  update(time, delta) {
     if (this.cursors.left.isDown) {
       this.ship.x -= 15
       if (this.ship.x < 0) {
