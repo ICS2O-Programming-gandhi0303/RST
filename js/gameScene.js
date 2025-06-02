d2bf7032352ab89e27f7a40ef179ff6a2a40661e
Dushime Gandhi <dushime.gandhi@stu.ocsb.ca>
2025-06-02 14:34:35 +0000

    Sentence about what you did

--- js/gameScene.js
+++ js/gameScene.js
@@ -4,12 +4,41 @@ class GameScene extends Phaser.Scene {
     let alienXVelocity = Math.floor(Math.random() * 50) + 1
     alienXVelocity *= Math.round(Math.random()) ? -1 : 1 
     const anAlien = this.physics.add.sprite(alienXLocation, -100, 'alien')
-    anAlien.body.velocity.y = 200 // Makes the alien fall down
+    anAlien.body.velocity.y = 200
     anAlien.body.velocity.x = alienXVelocity
     this.alienGroup.add(anAlien)
   }
+  constructor() {
+    super({ key: 'gameScene' })
 
-  create(data) {
+    this.background = null
+    this.ship = null
+    this.fireMissile = false
+    this.missileGroup = null
+    this.alienGroup = null
+    this.cursors = null
+    this.keySpaceObj = null
+    this.score = 0
+    this.scoreText = null
+    this.scoreTextStyle = { font: '65px Arial', fill: '#ffffff', align: 'center' }
+  }
+
+  init() {
+    this.cameras.main.setBackgroundColor('#ffffff')
+  }
+
+  preload() {
+    console.log('Game Scene')
+    this.load.image("starBackground", "./assets/starBackground.png")
+    this.load.image("ship", "./assets/spaceShip.png")
+    this.load.image("missile", "./assets/missile.png")
+    this.load.image("alien", "./assets/alien.png")
+    // Load the sound effect for the laser
+    this.load.audio("laser", "./assets/laser1.wav")
+    this.load.audio("explosion", "./assets/barrelExploding.wav")
+  }
+
+  create() {
     this.background = this.add.image(0, 0, 'starBackground').setScale(2.0)
     this.background.setOrigin(0, 0)
     this.scoreText = this.add.text(10, 10, 'Score: '+ this.score.toString(), this.scoreTextStyle)
@@ -17,10 +46,61 @@ class GameScene extends Phaser.Scene {
     this.ship.body.allowGravity = false
     this.missileGroup = this.physics.add.group()
     this.alienGroup = this.physics.add.group()
-    // Now it's safe to create aliens
+    // Ensure groups are initialized before creating aliens
     this.createAlien()
     this.createAlien()
-    // ... rest of your code ...
+ 
+    this.physics.add.collider(this.missileGroup, this.alienGroup, function (_, alienCollide) {
+      alienCollide.destroy()
+      this.sound.play("explosion")
+      this.score = this.score + 1
+      this.scoreText.setText('Score: ' + this.score.toString())
+      this.createAlien()
+      this.createAlien()
+    }.bind(this))
   }
-  // ... rest of your class ...
+
+  update() {
+    if (this.cursors.left.isDown) {
+      this.ship.x -= 15
+      if (this.ship.x < 0) {
+        this.ship.x = 0
+      }
+    }
+    if (this.cursors.right.isDown) {
+      this.ship.x += 15
+      if (this.ship.x > 1920) {
+        this.ship.x = 1920
+      }
+    }
+    if (this.keySpaceObj.isDown) {
+      if (!this.fireMissile) {
+        this.fireMissile = true
+      
+        const aNewMissile = this.physics.add.sprite(this.ship.x, this.ship.y, 'missile')
+        this.missileGroup.add(aNewMissile)
+        aNewMissile.body.allowGravity = false
+        aNewMissile.body.velocity.y = -600
+        this.sound.play("laser")
+      }
+    }
+    if (this.keySpaceObj.isUp) {
+      this.fireMissile = false
+    }
+    // Destroy missiles that go off the top of the screen
+    this.missileGroup.getChildren().forEach(function(item) {
+      if (item.active && item.y < 0) {
+        item.destroy()
+      }
+    });
+
+    // Destroy aliens that go off the bottom of the screen and respawn
+    this.alienGroup.getChildren().forEach(function(alien) {
+      if (alien.active && alien.y > 1080) {
+        alien.destroy();
+        this.createAlien();
+      }
+    }, this);
+  }
 }
+export default GameScene;
